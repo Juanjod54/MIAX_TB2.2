dccc9678b9a00789bba3601b3bc18dc89a25b44a
Juanjo <juanjod54@gmail.com>
2025-12-08 18:26:19 +0100

    Improvements

--- src/analyzer.py
+++ src/analyzer.py
@@ -2,11 +2,12 @@ import pandas as pd
 
 def __consolidate_mics__(dataframe: pd.DataFrame, mics) -> pd.DataFrame:
     tolerance = pd.Timedelta(microseconds=1000)
-    columns = ['mic', 'px_ask_0', 'px_bid_0', 'qty_ask_0', 'qty_bid_0', 'ord_ask_0', 'ord_bid_0']
-    _dataframe = dataframe.copy()[columns]
+    _dataframe = dataframe.copy()[['mic', 'px_ask_0', 'px_bid_0', 'qty_ask_0', 'qty_bid_0']]
     _dataframe['date_time'] = pd.to_datetime(dataframe['epoch'].astype('int64'), unit='us')
-    _dataframe = _dataframe.drop_duplicates(subset='date_time', keep='last')
-    bests_df = pd.DataFrame(_dataframe['date_time'])
+
+    # Ordenamos por el momento en el que se registro el evento y quitamos duplicados para tener un df limpio
+    _dataframe = _dataframe.sort_values('date_time')
+    bests_df = pd.DataFrame(_dataframe['date_time']).drop_duplicates(subset='date_time', keep='last')
     for mic in mics:
 
         # El cambio de moneda varia mas rapido que el de los precios por lo que no deberia
@@ -21,17 +22,7 @@ def __consolidate_mics__(dataframe: pd.DataFrame, mics) -> pd.DataFrame:
         # En el libro de ordenes las filas vienen ordenadas
         # bid 0 -> mas alta, ask 0 -> mas baja
 
-        mic_df = _dataframe[_dataframe['mic'] == mic]
-
-        ## In a simulation, if an opportunity persists for 1 second (1000 snapshots), ##
-        ## you can only trade it once (the first time it appears) ##
-
-        # Calculamos la diferencia de segundos entre filas
-        time_diff = mic_df['date_time'].diff().dt.total_seconds()
-        # Comprobamos si todas las columnas son iguales y el tiempo es inferior a 1 segundo
-        is_duplicated = (mic_df[columns] == mic_df[columns].shift()).all(axis=1) & (time_diff < 1)
-        # Descartamos los valores
-        mic_df = mic_df[~is_duplicated]
+        mic_df = _dataframe[_dataframe['mic'] == mic].drop_duplicates(subset='date_time', keep='last')
 
         best_asks = mic_df['px_ask_0']
         best_bids = mic_df['px_bid_0']
@@ -48,7 +39,6 @@ def __consolidate_mics__(dataframe: pd.DataFrame, mics) -> pd.DataFrame:
         )
 
         bests_df = bests_df.sort_values('date_time')
-        consolidated = consolidated.sort_values('date_time')
         bests_df = pd.merge_asof(bests_df, consolidated, on='date_time', direction='backward', tolerance=tolerance)
 
     mics_df = bests_df.set_index('date_time')
@@ -61,34 +51,41 @@ def find_arbitrage(dataframe: pd.DataFrame, latency=0):
     # Formamos el dataframe con el indice = epoch y la mejor orden de compra y venta por mic, con una tolerancia de hasta 1 ms entre mics
     consolidated_dataframe = __consolidate_mics__(dataframe, mics)
 
+    # Aplicamos la latencia
+    # Sustituimos los valores de compra (a los que les afecta la latencia) por sus valores en 'latency' microsegundos
+    # shifted_bids = possible_arbitrages[best_bid_cols]
+    # shifted_bids.index = shifted_bids.index + pd.Timedelta(microseconds=latency)
+    # shifted_bids = shifted_bids.reindex(possible_arbitrages.index, fill_value=0)
+    # possible_arbitrages[best_bid_cols] = shifted_bids
+
     min_ask_per_epoch = consolidated_dataframe[best_ask_cols].min(axis=1)
     max_bid_per_epoch = consolidated_dataframe[best_bid_cols].max(axis=1)
+
     # Nos quedamos con los que pueden ser arbitrajes para reducir la busqueda
     bid_gt_ask = (max_bid_per_epoch > min_ask_per_epoch)
     bid_gt_ask = bid_gt_ask.reindex(consolidated_dataframe.index, fill_value=False)
     possible_arbitrages = consolidated_dataframe[bid_gt_ask]
+
     # Obtenemos donde estan los min ask y max bid por epoch
     min_ask_mic_per_epoch = possible_arbitrages[best_ask_cols].idxmin(axis=1, skipna=True)
     max_bid_mic_per_epoch = possible_arbitrages[best_bid_cols].idxmax(axis=1, skipna=True)
+
     # Sacamos los mics
     min_ask_mic_per_epoch = min_ask_mic_per_epoch.apply(lambda v: v.split('_')[0] if pd.notna(v) else None)
     max_bid_mic_per_epoch = max_bid_mic_per_epoch.apply(lambda v: v.split('_')[0] if pd.notna(v) else None)
+
     # Nos quedamos solo con los que no sean del mismo mic
     different_mic = (max_bid_mic_per_epoch != min_ask_mic_per_epoch)
     different_mic = different_mic.reindex(possible_arbitrages.index, fill_value=False)
     possible_arbitrages = possible_arbitrages[different_mic]
 
-    # Aplicamos la latencia. Es el supuesto en el que hemos visto las ordenes, hemos decidido actuar y ahora tenemos latencia.
-    # Sustituimos los valores de compra (a los que les afecta la latencia) por sus valores en 'latency' microsegundos
-    shifted_bids = possible_arbitrages[best_bid_cols]
-    shifted_bids.index = shifted_bids.index + pd.Timedelta(microseconds=latency)
-    shifted_bids = shifted_bids.reindex(possible_arbitrages.index, fill_value=0)
-    possible_arbitrages[best_bid_cols] = shifted_bids
-
+    last_arbitrage = None
+    first_occur_at = None
+    max_delay = pd.Timedelta(seconds=1)
     arbitrage_cols = ['Ask', 'Bid', 'From', 'To', 'Volume', 'Profit']
     arbitrages = pd.DataFrame(columns=arbitrage_cols, index=possible_arbitrages.index)
-
     for epoch, row in possible_arbitrages.iterrows():
+
         # Sacamos el mic donde esta el menor precio de venta
         ask_mic = min_ask_mic_per_epoch[epoch]
         # Sacamos el mic donde esta el mayor precio de compra
@@ -99,10 +96,40 @@ def find_arbitrage(dataframe: pd.DataFrame, latency=0):
         ask_qty = row[f'{ask_mic}_best_ask_volume']
         bid_qty = row[f'{bid_mic}_best_bid_volume']
 
-        arbitrages.loc[epoch] = [min_ask, max_bid, ask_mic, bid_mic, min(ask_qty, bid_qty), (max_bid - min_ask) * min(ask_qty, bid_qty)]
+        arbitrage = [min_ask, max_bid, ask_mic, bid_mic, min(ask_qty, bid_qty),
+                                 (max_bid - min_ask) * min(ask_qty, bid_qty)]
 
-    # Eliminamos las filas que son iguales a la siguiente puesto que es una oportunidad duplicada
-    arbitrages['Rising Edge'] = (arbitrages[arbitrage_cols] != arbitrages[arbitrage_cols].shift(1)).any(axis=1)
-    arbitrages = arbitrages[arbitrages['Rising Edge']].drop(columns=['Rising Edge'])
+        ################################################################################
+        ## In a simulation, if an opportunity persists for 1 second (1000 snapshots), ##
+        ## you can only trade it once (the first time it appears)                     ##
+        ## if the opportunity vanishes and quickly reappears you can count it         ##
+        ## as a new opportunity for simplification                                    ##
+        ################################################################################
+
+        if (last_arbitrage is None) or (last_arbitrage != arbitrage) or ((first_occur_at + max_delay) < epoch):
+            first_occur_at = epoch
+            last_arbitrage = arbitrage
+
+            arbitrages.loc[epoch] = arbitrage
+
+    ################################################################################
+
+    ################################################################################
+    ##                              Alternativa                                   ##
+    ################################################################################
+    ## In a simulation, if an opportunity persists for 1 second (1000 snapshots), ##
+    ## you can only trade it once (the first time it appears)                     ##
+    ## if the opportunity vanishes and quickly reappears you can count it         ##
+    ## as a new opportunity for simplification                                    ##
+    ################################################################################
+
+    # Eliminamos filas repetidas en menos de 1 segundo
+    # time_delta = possible_arbitrages.index.to_series().diff()
+    # duplicated = (arbitrages[['Ask', 'Bid', 'From', 'To', 'Volume', 'Profit']] == arbitrages[['Ask', 'Bid', 'From', 'To', 'Volume', 'Profit']].shift(1)).all(axis=1)
+    # arbitrages = arbitrages[~ (duplicated & time_delta < max_delay)]
+    ################################################################################
+
     return arbitrages
+
+
 
